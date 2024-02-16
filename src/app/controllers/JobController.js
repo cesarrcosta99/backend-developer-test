4ab5fcc4987f8061ca7c221cbaf7a4f96db80381
cesarrcosta99 <cesarrcosta99@gmail.com>
2024-02-15 21:51:45 -0300

    Adicão a funcionalidade de atualização ao JobController e Implementação a funcionalidade de exclusão de trabalho no JobController

--- src/app/controllers/JobController.js
+++ src/app/controllers/JobController.js
@@ -7,7 +7,7 @@ class JobController {
       title: Yup.string().required(),
       description: Yup.string().required(),
       location: Yup.string().required(),
-      company_id: Yup.number().required(),
+      company_id: Yup.string().required(),
       notes: Yup.string().required(),
       status: Yup.string(),
     });
@@ -18,7 +18,8 @@ class JobController {
       return response.status(400).json({ error: err.errors });
     }
 
-    const { title, description, location, company_id, notes, status } =request.body;
+    const { title, description, location, company_id, notes, status } =
+      request.body;
 
     const job = await Job.create({
       title,
@@ -29,7 +30,72 @@ class JobController {
       status: status || "draft",
     });
 
-    return response.status(201).json(job)
+    return response.status(201).json(job);
+  }
+
+  async publish(request, response) {
+    const { job_id } = request.params;
+    try {
+      const job = await Job.findByPk(job_id);
+      if (!job) {
+        return response.status(404).json({ error: "Job not found" });
+      }
+
+      await job.update({ status: "published" });
+
+      return response.json(job);
+    } catch (err) {
+      return response.status(500).json({ error: err.message });
+    }
+  }
+
+  async update(request, response) {
+    const schema = Yup.object().shape({
+      title: Yup.string(),
+      description: Yup.string(),
+      location: Yup.string(),
+    });
+
+    try {
+      await schema.validateSync(request.body, { abortEarly: false });
+    } catch (err) {
+      return response.status(400).json({ error: err.errors });
+    }
+
+    const { job_id } = request.params;
+    const { title, description, location } = request.body;
+
+    try {
+      const job = await Job.findByPk(job_id);
+
+      if (!job) {
+        return response.status(404).json({ error: "Job not found" });
+      }
+
+      await job.update({ title, description, location });
+
+      return response.json(job);
+    } catch (err) {
+      return response.status(500).json({ error: err.message });
+    }
+  }
+
+  async delete(request, response) {
+    const { job_id } = request.params;
+
+    try {
+      const job = await Job.findByPk(job_id);
+
+      if (!job) {
+        return response.status(404).json({ error: "Job not found" });
+      }
+
+      await job.destroy();
+
+      return response.status(204).send();
+    } catch (err) {
+      return response.status(500).json({ error: err.message });
+    }
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import CompanyController from "./app/controllers/CompanyController.js";
+import JobController from "./app/controllers/JobController.js";
 
 const routes = new Router();
 
 routes.get("/companies", CompanyController.index);
 routes.get("/companies/:company_id", CompanyController.show);
+routes.post("/job", JobController.store);
+routes.put("/job/:job_id/publish", JobController.publish);
+routes.put("/job/:job_id", JobController.update);
+routes.delete("/job/:job_id", JobController.delete);
 
 export default routes;
